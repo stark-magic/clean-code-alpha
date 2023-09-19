1cced688a75f65b4c3b7f024cf36b668ac107131
Peter McKee <peter.mckee@gmail.com>
2023-09-19 09:08:20 -0500

    fixing the code

--- server/server.js
+++ server/server.js
@@ -1,10 +1,10 @@
 const ronin 		= require( 'ronin-server' )
 const mocks 		= require( 'ronin-mocks' )
 const UrlHelper		= require( './util/urlHelper' )
+const dbHelper		= require( './util/dbHelper' )
 const mongo 		= require( 'mongodb' ).MongoClient
 
 async function main() {
-	applicationName = "Clean Code Application";
 	try {
 	//
 	// This is were we setup the server
@@ -13,17 +13,9 @@ async function main() {
 			port: process.env.PORT || 8080
 		})
 
-		// This should change
-		// const db = await mongo.connect( "mongodb+srv://myDatabaseUser:D1fficultP%41ssw0rd@cluster0.example.mongodb.net/?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true } )
-		const db = await mongo.connect( "mongodb+srv://myDatabaseUser:D1fficultP%40ssw0rd@cluster0.example.mongodb.net/?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true } )
+		await dbHelper.getDbConnection()
 
 	  server.use( '/services/', mocks.server( server.Router(), false, true ) )
-	  server.use( '/services/v2/people', function( req, res ) {
-		let sql = "SELECT * FROM "+ req.params.sql;
-
-		const urlHelper = UrlHelper();
-		console.info( 'urlHelper: '+ urlHelper.url )
-	  })
 
     const result = await server.start()
     console.info( result )
--- server/util/urlHelper.js
+++ server/util/urlHelper.js
@@ -3,6 +3,7 @@ function UrlHelper( url ) {
     let myUrls = new Array(x1, x2, x3)
     this.url = url
     myUrls.push( url )
+    return myUrls
 }
 
 function parseUrl( url ) {
